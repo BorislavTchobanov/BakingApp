fa7f7fe727ec1fd2c534062ce777a2193141b940
DESKTOP-S09G547\borislav <botchobanov@gmail.com>
2018-11-19 22:50:13 +0200

    test

--- app/src/main/java/com/example/android/bakingapp/StepsAdapter.java
+++ app/src/main/java/com/example/android/bakingapp/StepsAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.bakingapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +39,7 @@ public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepViewHold
         int layoutIdForListItem = R.layout.step_list_content;
         LayoutInflater inflater = LayoutInflater.from(context);
 
+        Log.i("TEST", "TEST");
         View view = inflater.inflate(layoutIdForListItem, viewGroup, false);
 
         return new StepViewHolder(view);
